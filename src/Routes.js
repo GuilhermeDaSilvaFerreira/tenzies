15330d9efab41adb7847de41e8ab4dd18e2c3492
Guilherme Ferreira <guilhermesilva.ferreira@hotmail.com>
2022-04-02 23:14:29 -0300

    Added ReactRouter to make GitHub Pages work

---
+++ src/Routes.js
@@ -0,0 +1,12 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import App from './App'
+
+export default function AppRoutes() {
+    return(
+        <Router>
+            <Routes>
+                <Route path="/tenzies" element={<App />} />
+            </Routes>
+        </Router>
+    )
+}
